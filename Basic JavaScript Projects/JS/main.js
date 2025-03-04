9e6c2023ab68c339be0b13156fdbb3057e749114
CloudyGIF <anna22post@gmail.com>
2025-03-04 00:31:51 +0000

    excersise

---
+++ Basic JavaScript Projects/JS/main.js
@@ -0,0 +1,4 @@
+window.alert('This is my alert!'); //window alert
+var Sent1 = "This is the beginning of the string"; //sets var sent1
+var Sent2 = " and this is the end of the string"; //sets var sent2
+document.write(Sent1 + Sent2); //prints concatenated sent1 + sent2
