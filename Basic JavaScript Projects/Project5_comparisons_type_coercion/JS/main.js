58d08b52c35dd82682fceec0db5de338c3d9b6f1
CloudyGIF <anna22post@gmail.com>
2025-03-06 22:35:42 +0000

    /

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,36 @@
+var string = '10';
+var int = 3;
+
+document.write(string +  int); //combining a string and an intiger
+document.write("   ");
+
+document.write(3E310);
+document.write("   ");
+
+document.write(-3E310); //infinities
+document.write("   ");
+
+document.write(10 > 3);
+document.write("   ");
+
+document.write(10 < 3);
+document.write("   ");
+document.write(10 == 3);
+document.write("   ");
+document.write(10 == 10);
+document.write("   ");
+document.write(10 === 10);
+document.write("   ");
+document.write(10 === "7");
+document.write("   ");
+document.write(10 === "10");
+document.write("   ");
+document.write(10 === "10" && 10 < 3);
+document.write("   ");
+document.write(10 === "10" || 10 == 10);
+document.write("   ");
+document.write(!(2>1));
+document.write("   ");
+document.write(!(2<1)); //various true false expression returns
+
+console.log(7 < 2) //console log statement
